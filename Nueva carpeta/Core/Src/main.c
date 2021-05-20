d7a6accdf14b4b9077bf20fdabcb6c79ccee0820
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-05-19 22:48:58 -0500

    c

--- Nueva carpeta/Core/Src/main.c
+++ Nueva carpeta/Core/Src/main.c
@@ -65,6 +65,7 @@ void mi_funcion(uint16_t, int);
 void StartTask1(void *argument);
 void StartTask2(void *argument);
 void StartTask3(void *argument);
+void StartTask4(void *argument);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -251,7 +252,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -259,8 +260,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA4 PA5 PA6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+  /*Configure GPIO pins : PA0 PA4 PA5 PA6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -294,7 +295,7 @@ void StartTask1(void *argument)
 {
   /* USER CODE BEGIN 5 */
 
-  TaskHandle_t prioridadTarea1 = uxTaskPriorityGet(taskHandle1);
+  UBaseType_t prioridadTarea1 = uxTaskPriorityGet(taskHandle1);
   xTaskCreate(
     StartTask2,
     (const char*) "task2",
