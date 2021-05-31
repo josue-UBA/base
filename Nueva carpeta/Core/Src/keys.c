c11c2adc279eac2704a80496e9b38f80603411a6
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-05-30 22:31:23 -0500

    Revert "Revert "aa""
    This reverts commit 15fb731cdaf8085459ad4347a156f37c08530178.

--- Nueva carpeta/Core/Src/keys.c
+++ Nueva carpeta/Core/Src/keys.c
@@ -1,21 +1,21 @@
-/*=============================================================================
- * Copyright (c) 2021, Franco Bucafusco <franco_bucafusco@yahoo.com.ar>
- * 					   Martin N. Menendez <mmenendez@fi.uba.ar>
- * All rights reserved.
- * License: Free
- * Date: 2021/10/03
- * Version: v1.2
- *===========================================================================*/
+/* 1 Includes ------------------------------------------------------------------*/
+/* 2 Private includes ----------------------------------------------------------*/
+/* 3 Private typedef -----------------------------------------------------------*/
+/* 4 Private define ------------------------------------------------------------*/
+/* 5 Private macro -------------------------------------------------------------*/
+/* 6 Private variables ---------------------------------------------------------*/
+/* 7 Private function prototypes -----------------------------------------------*/
+/* 8 Private user code ---------------------------------------------------------*/
 
-/*==================[ Inclusions ]============================================*/
 #include "FreeRTOS.h"
 #include "task.h"
 #include "keys.h"
 #include "main.h"
 #include "sapi_peripheral_map.h"
 #include "sapi_button.h"
+#include "semphr.h"
 /*=====[ Definitions of private data types ]===================================*/
-const t_key_config  keys_config[] = { TEC1 };
+const t_key_config  keys_config [] = { TEC1, TEC2, TEC3, TEC4 };
 
 #define KEY_COUNT   sizeof(keys_config)/sizeof(keys_config[0])
 /*=====[Definition macros of private constants]==============================*/
@@ -33,22 +33,27 @@ GPIO_PinState gpioRead(gpioMap_t);
 
 /*=====[Definitions of public global variables]==============================*/
 t_key_data keys_data[KEY_COUNT];
-
+extern SemaphoreHandle_t sem_btn;
 /*=====[prototype of private functions]=================================*/
 void task_tecla( void* taskParmPtr );
 
 /*=====[Implementations of public functions]=================================*/
 TickType_t get_diff( uint32_t index )
 {
-    TickType_t tiempo;
-    tiempo = keys_data[index].time_diff;
+  TickType_t tiempo;
 
-    return tiempo;
+  taskENTER_CRITICAL();
+  tiempo = keys_data[index].time_diff;
+  taskEXIT_CRITICAL();
+
+  return tiempo;
 }
 
 void clear_diff( uint32_t index )
 {
-    keys_data[index].time_diff = KEYS_INVALID_TIME;
+  taskENTER_CRITICAL();
+  keys_data[index].time_diff = KEYS_INVALID_TIME;
+  taskEXIT_CRITICAL();
 }
 
 void keys_Init( void )
@@ -137,20 +142,31 @@ void keys_Update( uint32_t index )
 static void buttonPressed( uint32_t index )
 {
   TickType_t current_tick_count = xTaskGetTickCount();
+  taskENTER_CRITICAL();
   keys_data[index].time_down = current_tick_count;
+  taskEXIT_CRITICAL();
 }
 
 /* accion de el evento de tecla liberada */
 static void buttonReleased( uint32_t index )
 {
   TickType_t current_tick_count = xTaskGetTickCount();
+  taskENTER_CRITICAL();
   keys_data[index].time_up    = current_tick_count;
   keys_data[index].time_diff  = keys_data[index].time_up - keys_data[index].time_down;
+  taskEXIT_CRITICAL();
+
+  if ( keys_data[index].time_diff  > 0 )
+  {
+    xSemaphoreGive( sem_btn );
+  }
 }
 
 static void keys_ButtonError( uint32_t index )
 {
+  taskENTER_CRITICAL();
   keys_data[index].state = BUTTON_UP;
+  taskEXIT_CRITICAL();
 }
 
 /*=====[Implementations of private functions]=================================*/
@@ -173,9 +189,21 @@ GPIO_PinState gpioRead(gpioMap_t pin)
   {
     return HAL_GPIO_ReadPin(GPIOA, IN_1);
   }
+  else if( pin == TEC2 )
+  {
+	return HAL_GPIO_ReadPin(GPIOA, IN_2);
+  }
+  else if( pin == TEC3 )
+  {
+	return HAL_GPIO_ReadPin(GPIOA, IN_3);
+  }
+  else if( pin == TEC4 )
+  {
+	return HAL_GPIO_ReadPin(GPIOA, IN_4);
+  }
   else
   {
-	return GPIO_PIN_SET;
+    return GPIO_PIN_RESET;
   }
-
 }
+
