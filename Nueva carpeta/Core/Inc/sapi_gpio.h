3ae5d097bb18e4f8eef626a84287c3ae250ce204
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-06-09 00:43:23 -0500

    x

--- Nueva carpeta/Core/Inc/sapi_gpio.h
+++ Nueva carpeta/Core/Inc/sapi_gpio.h
@@ -12,5 +12,10 @@
 #include "main.h"
 void gpioWrite(gpioMap_t, int);
 int gpioRead(gpioMap_t);
+void gpioToggle(gpioMap_t);
+void mi_printf(char *, char*);
+
+extern UART_HandleTypeDef huart2;
+
 
 #endif /* INC_SAPI_GPIO_H_ */
