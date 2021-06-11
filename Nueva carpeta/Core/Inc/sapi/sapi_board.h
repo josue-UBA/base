549326f240444df28166bd16f7373326e273541c
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-06-10 20:22:21 -0500

    x

---
+++ Nueva carpeta/Core/Inc/sapi/sapi_board.h
@@ -0,0 +1,6 @@
+
+#include "main.h"
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+void boardConfig(); // se agrega
--- Nueva carpeta/Core/Src/main.c
+++ Nueva carpeta/Core/Src/main.c
@@ -1,252 +1,230 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+/*
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-UART_HandleTypeDef huart2;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
+  init();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
+*/
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
+/*=============================================================================
+ * Copyright (c) 2021, Franco Bucafusco <franco_bucafusco@yahoo.com.ar>
+ * 					   Martin N. Menendez <mmenendez@fi.uba.ar>
+ * All rights reserved.
+ * License: Free
+ * Date: 2021/10/03
+ * Version: v1.2
+ *===========================================================================*/
+
+/*==================[inclusiones]============================================*/
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "FreeRTOSConfig.h"
+
+#include "sapi/sapi.h"
+
+/*==================[definiciones y macros]==================================*/
+#define LED_RATE_MS 500
+#define LOADING_RATE_MS 250
+
+#define LED_RATE pdMS_TO_TICKS(LED_RATE_MS)
+#define LOADING_RATE pdMS_TO_TICKS(LOADING_RATE_MS)
+
+#define MODE 3
+
+/*==================[definiciones de datos internos]=========================*/
+
+/*==================[definiciones de datos externos]=========================*/
+
+DEBUG_PRINT_ENABLE;
+
+/*==================[declaraciones de funciones internas]====================*/
+
+/*==================[declaraciones de funciones externas]====================*/
+
+// Prototipo de funcion de la tarea
+void heart_beat( void* taskParmPtr ); // Prendo A - Apago A
+void loading_1( void* taskParmPtr );  // Prendo A - Apago A - Prendo B - Apago B .... hasta D
+void loading_2( void* taskParmPtr ); // Prendo A - Prendo B - ... - Apago A - Apago B - ...
+
+/*==================[funcion principal]======================================*/
+
+// FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
+int main( void )
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
+    // ---------- CONFIGURACIONES ------------------------------
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+    boardConfig();									// Inicializar y configurar la plataforma
+
+    gpioInit( GPIO0, GPIO_OUTPUT ); // Puedo usar cualquiera del GPIO0 al GPIO8
+    gpioInit( GPIO7, GPIO_OUTPUT );
+    gpioInit( GPIO5, GPIO_OUTPUT );
+    gpioInit( GPIO3, GPIO_OUTPUT );
+    gpioInit( GPIO1, GPIO_OUTPUT );
+
+    debugPrintConfigUart( UART_USB, 115200 );		// UART for debug messages
+    printf( "Ejercicio B_1.\r\n" );
+
+    //gpioWrite( LED3, ON );							// Led para dar se�al de vida
+
+#if MODE == 1
+    // Crear tarea en freeRTOS
+    BaseType_t res =
+        xTaskCreate(
+            heart_beat,                     	// Funcion de la tarea a ejecutar
+            ( const char * )"heart_beat",   	// Nombre de la tarea como String amigable para el usuario
+            configMINIMAL_STACK_SIZE*2, 		// Cantidad de stack de la tarea
+            0,                          		// Parametros de tarea
+            tskIDLE_PRIORITY+1,         		// Prioridad de la tarea -> Queremos que este un nivel encima de IDLE
+            0                          			// Puntero a la tarea creada en el sistema
+        );
+#endif
+
+#if MODE == 2
+    // Crear tarea en freeRTOS
+    BaseType_t res =
+        xTaskCreate(
+            loading_1,                     	// Funcion de la tarea a ejecutar
+            ( const char * )"loading_1",   	// Nombre de la tarea como String amigable para el usuario
+            configMINIMAL_STACK_SIZE*2, 		// Cantidad de stack de la tarea
+            0,                          		// Parametros de tarea
+            tskIDLE_PRIORITY+1,         		// Prioridad de la tarea -> Queremos que este un nivel encima de IDLE
+            0                          			// Puntero a la tarea creada en el sistema
+        );
+#endif
+
+
+#if MODE == 3
+    // Crear tarea en freeRTOS
+    BaseType_t res =
+        xTaskCreate(
+            loading_2,                     	// Funcion de la tarea a ejecutar
+            ( const char * )"loading_2",   	// Nombre de la tarea como String amigable para el usuario
+            configMINIMAL_STACK_SIZE*2, 		// Cantidad de stack de la tarea
+            0,                          		// Parametros de tarea
+            tskIDLE_PRIORITY+1,         		// Prioridad de la tarea -> Queremos que este un nivel encima de IDLE
+            0                          			// Puntero a la tarea creada en el sistema
+        );
+#endif
+    // Gestion de errores
+    //if(res == pdFAIL)
+    //{
+    //	gpioWrite( LEDR, ON );
+    //	printf( "Error al crear las tareas.\r\n" );
+    //	while(TRUE);						// VER ESTE LINK: https://pbs.twimg.com/media/BafQje7CcAAN5en.jpg
+    //}
+    configASSERT( res == pdPASS ); // gestion de errores
+
+    // Iniciar scheduler
+    vTaskStartScheduler(); // Enciende tick | Crea idle y pone en ready | Evalua las tareas creadas | Prioridad mas alta pasa a running
+
+    // ---------- REPETIR POR SIEMPRE --------------------------
+    //while( TRUE )
+    //{
+    // Si cae en este while 1 significa que no pudo iniciar el scheduler
+    //}
+    configASSERT( 0 );
+
+    // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
+    // directamenteno sobre un microcontroladore y no es llamado por ningun
+    // Sistema Operativo, como en el caso de un programa para PC.
+
+
+    return TRUE;
+}
+
+/*==================[definiciones de funciones internas]=====================*/
+
+/*==================[definiciones de funciones externas]=====================*/
+
+// Implementacion de funcion de la tarea
+void heart_beat( void* taskParmPtr )
+{
+    // ---------- CONFIGURACIONES ------------------------------
+    gpioWrite( GPIO0, ON );
+    // ---------- REPETIR POR SIEMPRE --------------------------
+    while( TRUE )
+    {
+        gpioWrite( LEDB, ON );
+        vTaskDelay( LED_RATE_MS / portTICK_RATE_MS );
+
+        gpioWrite( LEDB,OFF );
+        gpioToggle( GPIO0 );
+
+        vTaskDelay( LED_RATE ); //NO USAR!!
+    }
+}
+
+// Implementacion de funcion de la tarea
+void loading_1( void* taskParmPtr )
+{
+    // ---------- CONFIGURACIONES ------------------------------
+    gpioMap_t led = LEDB;
+    gpioMap_t gpio = GPIO7;
+
+    gpioWrite( GPIO7, ON );
+    gpioWrite( GPIO5, ON );
+    gpioWrite( GPIO3, ON );
+    gpioWrite( GPIO1, ON );
+    // ---------- REPETIR POR SIEMPRE --------------------------
+    while( TRUE )
+    {
+        gpioWrite( led, ON );
+        gpioWrite( gpio, ON );
+        vTaskDelay( LOADING_RATE );
+
+        gpioWrite( led,OFF );
+        gpioWrite( gpio, OFF );
+
+        if ( led == LED3 )
+        {
+            led = LEDB;
+            gpio = GPIO7;
+        }
+        else
+        {
+            led = led + 1;
+            gpio ++;
+        }
+
+        vTaskDelay( LOADING_RATE ); //NO USAR!!
+    }
+}
+
+// Implementacion de funcion de la tarea
+void loading_2( void* taskParmPtr )
+{
+    // ---------- CONFIGURACIONES ------------------------------
+    gpioMap_t led = LEDB;
+    gpioMap_t gpio = GPIO7;
+
+    gpioWrite( GPIO7, ON );
+    gpioWrite( GPIO5, ON );
+    gpioWrite( GPIO3, ON );
+    gpioWrite( GPIO1, ON );
+    // ---------- REPETIR POR SIEMPRE --------------------------
+    while( TRUE )
+    {
+        gpioToggle( led );
+        gpioToggle( gpio );
+
+        if ( led == LED3 )
+        {
+            led = LEDB;
+            gpio = GPIO7;
+        }
+        else
+        {
+            led = led + 1;
+            gpio ++;
+        }
+
+        vTaskDelay( LOADING_RATE );
+    }
+}
+
+/*==================[fin del archivo]========================================*/
