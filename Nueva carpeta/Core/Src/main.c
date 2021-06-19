f7fd782e39f0070f8451fb69804d98fddd3687ad
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-06-19 06:07:44 -0500

    Update main.c

--- Nueva carpeta/Core/Src/main.c
+++ Nueva carpeta/Core/Src/main.c
@@ -335,6 +335,23 @@ void vApplicationMallocFailedHook()
     configASSERT( 0 );
 }
 /*==================[fin del archivo]========================================*/
+int __io_putchar(int ch)
+{
+ uint8_t c[1];
+ c[0] = ch & 0x00FF;
+ HAL_UART_Transmit(&huart2, &*c, 1, 10);
+ return ch;
+}
+
+int _write(int file,char *ptr, int len)
+{
+ int DataIdx;
+ for(DataIdx= 0; DataIdx< len; DataIdx++)
+ {
+ __io_putchar(*ptr++);
+ }
+return len;
+}
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
