46053f34ebc70f22913e5ee6e9cf144c64b926ab
josueelias9 <30317390+josueelias9@users.noreply.github.com>
2021-06-18 22:21:41 -0500

    Update main.c

--- Nueva carpeta/Core/Src/main.c
+++ Nueva carpeta/Core/Src/main.c
@@ -27,9 +27,9 @@
 #include "FreeRTOSConfig.h"
 #include "keys.h"
 #include "sapi.h"
+#include "task.h"
 
 #include <stdio.h>
-#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -130,15 +130,15 @@ int main(void)
   /* add threads, ... */
   // Crear tarea en freeRTOS
   BaseType_t res;
+  printf( "Ejercicio D1\n" );
   res = xTaskCreate (
-    task_led,					// Funcion de la tarea a ejecutar
-    ( const char * )"task_led",	// Nombre de la tarea como String amigable para el usuario
-    configMINIMAL_STACK_SIZE*2,	// Cantidad de stack de la tarea
-    0,							// Parametros de tarea
-    tskIDLE_PRIORITY+1,			// Prioridad de la tarea
-    0							// Puntero a la tarea creada en el sistema
-  );
-
+            task_led,					// Funcion de la tarea a ejecutar
+            ( const char * )"task_led",	// Nombre de la tarea como String amigable para el usuario
+            configMINIMAL_STACK_SIZE*2,	// Cantidad de stack de la tarea
+            0,							// Parametros de tarea
+            tskIDLE_PRIORITY+1,			// Prioridad de la tarea
+            0							// Puntero a la tarea creada en el sistema
+        );
   // Gestión de errores
   configASSERT( res == pdPASS );
   /* USER CODE END RTOS_THREADS */
@@ -312,6 +312,24 @@ void vApplicationMallocFailedHook()
     printf( "Malloc Failed Hook!\n" );
     configASSERT( 0 );
 }
+int __io_putchar(int ch)
+{
+ uint8_t c[1];
+ c[0] = ch & 0x00FF;
+ HAL_UART_Transmit(&huart2, &*c, 1, 10);
+ return ch;
+}
+
+int _write(int file,char *ptr, int len)
+{
+ int DataIdx;
+ for(DataIdx= 0; DataIdx< len; DataIdx++)
+ {
+ __io_putchar(*ptr++);
+ }
+return len;
+}
+
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
